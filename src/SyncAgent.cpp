sailfishos/buteo-syncml

commit 83a7c4fb196bd62083ac928ddf9dd91b2fc5f10b

Fixes: NB#258951 - msyncd crash when cancel the sync from transfer-ui

src/SyncAgent.cpp
@@ -489,8 +489,6 @@ void SyncAgent::listenEvent()
 
     iListener->stop();
 
-    cleanListen();
-
     if( data.iType == RequestListener::REQUEST_CLIENT )
     {
         LOG_DEBUG( "SyncAgent: Remote client requesting session with server");

commit 493bea86f6c14448329cd4b938af436198d140bb

[SyncAgent] clean listen after single request
Regression introduced by 83a7c4fb196bd62083ac928ddf9dd91b2fc5f10b

Detected by SyncAgentTest::testSync(), SyncAgentTest.cpp:147

Relates NEMO#664

src/SyncAgent.cpp
@@ -488,6 +488,7 @@ void SyncAgent::listenEvent()
     RequestListener::RequestData data = iListener->takeRequestData();
 
     iListener->stop();
+    cleanListenLater();
 
     if( data.iType == RequestListener::REQUEST_CLIENT )
     {
@@ -619,5 +620,13 @@ void SyncAgent::cleanListen()
     FUNCTION_CALL_TRACE
 
     delete iListener;
+    iListener = NULL;
+}
+
+void SyncAgent::cleanListenLater()
+{
+    FUNCTION_CALL_TRACE
+
+    iListener->deleteLater();
     iListener = NULL;
 }

src/SyncAgent.h
@@ -745,6 +745,7 @@ private:
 
     void abortListen();
     void cleanListen();
+    void cleanListenLater();
 
     RequestListener*        iListener;
     SessionHandler*         iHandler;